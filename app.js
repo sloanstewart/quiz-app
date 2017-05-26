11aa7b8d34cf4ea69efdd427a600e685a41da4c0
Sloan <sloanstewartdev@gmail.com>
2017-05-26 09:31:26 -0400

    js update

--- app.js
+++ app.js
@@ -1,69 +1,61 @@
 // Create the question and answer objects
 function createQuizItems(){
-	function QuizItem(question, answers, correct) {
+	function QuizItem(question, answers) {
 		this.question = question;
 		this.answers = answers;
-		this.correct = correct;
 	};
 // can create array for all quiz items and not use constructor
 	var intro = new QuizItem('Welcome to this incredible quiz; now shut up and hang on.',
 		['Choose your answer carefully',
 		'There is only one correct answer per question.',
 		'Only the penitent man will pass.',
-		'Good luck!'],
-		null
+		'Good luck!']
 	);
 
 	var q1 = new QuizItem('What is Murphy\'s Law?',
-		['&quot;The road to success and the road to failure are almost exactly the same.&quot;', '&quot;Anything that can go wrong, will go wrong.&quot;', '&quot;Two wrongs don\'t make a right.&quot;', '&quot;Hope for the best, but prepare for the worst.&quot;'], '&quot;Anything that can go wrong, will go wrong.&quot;'
+		['&quot;The road to success and the road to failure are almost exactly the same.&quot;', '&quot;Anything that can go wrong, will go wrong.&quot;', '&quot;Two wrongs don\'t make a right.&quot;', '&quot;Hope for the best, but prepare for the worst.&quot;']
 	);
 
 	var q2 = new QuizItem('How can you tell, without question, if someone is allowed access?',
 		['They will have a proper badge or pass.',
 		'Security will let them in if it\'s ok.',
 		'They look like part of the band.',
-		'They look like they want to be elsewhere.'],
-		'They look like they want to be elsewhere.'
+		'They look like they want to be elsewhere.']
 	);
 
 	var q3 = new QuizItem('As an audio tech, how can you avoid injury lifting heavy equipment?',
 		['Label all heavy items as &quot;Lighting&quot;.',
 		'Lift with your legs, never your back.',
 		'Have someone help you lift.',
-		'Try harder.'],
-		'Label all heavy items as &quot;Lighting&quot;.'
+		'Try harder.']
 	);
 
 	var q4 = new QuizItem('Video content is on the way, when should you test it?',
 		['As soon as possible.',
 		'1 hour before show.',
 		'10 minutes before show.',
-		'Tell the client &quot;No&quot;.'],
-		'Tell the client &quot;No&quot;.'
+		'Tell the client &quot;No&quot;.']
 	);
 
 	var q5 = new QuizItem('How do you make money in the music business?',
 		['Hard work and dedication.',
 		'Sell beer.',
 		'Networking -It\'s who you know that counts.',
-		'Knowledge - Folks will value your skills.'],
-		'Sell beer.'
+		'Knowledge - Folks will value your skills.']
 	);
 
 	var q6 = new QuizItem('If a patron requests a change to the mix, what should you do?',
 		['Ignore them.',
 		'Pretend to make an adjustment.',
 		'Have security escort them out of the event.',
-		'All of the above.'],
-		'All of the above.'
+		'All of the above.']
 	);
 
 	var q7 = new QuizItem('Lighting is taking a long time to load out, what should you do?',
 		['Jump in and help wrap some cables.',
 		'Try to catch a nap or have a snack.',
 		'Offer to help tear down fixtures.',
-		'Push cases to the truck.'],
-		'Try to catch a nap or have a snack.'
+		'Push cases to the truck.']
 	);
 
 	var q8 = new QuizItem('When should you schedule your lunch or dinner break?',
@@ -77,15 +69,10 @@ function createQuizItems(){
 		['Make sure you have a microphone for it.',
 		'Add some low end EQ for maximum impact.',
 		'Throw it in the garbage.',
-		'Make sure it is tuned properly.'],
-		'Throw it in the garbage.'
+		'Make sure it is tuned properly.']
 	);
 
 	var q10 = new QuizItem('When was the last time you got enough sleep?',
-		['Huh?',
-		'What?',
-		'I dunno.',
-		'Eh.'],
 		['Huh?',
 		'What?',
 		'I dunno.',
@@ -103,9 +90,7 @@ function createQuizItems(){
 		q8.question + '</br><span class=youranswer id=q8></span>',
 		q9.question + '</br><span class=youranswer id=q9></span>',
 		q10.question + '</br><span class=youranswer id=q10></span>',
-		],
-		null
-
+		]
 		// ^ iterate through questions and check CORRECT answers
 	);
 	return [intro, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, end];
@@ -179,7 +164,7 @@ var yourAnswers = {
 		$('#q5').text(yourAnswers.q5);
 		$('#q6').text(yourAnswers.q6);
 		$('#q7').text(yourAnswers.q7);
-		$('#q8').text(yourAnswers.q1);
+		$('#q8').text(yourAnswers.q8);
 		$('#q9').text(yourAnswers.q9);
 		$('#q10').text(yourAnswers.q10);
 	}
@@ -193,10 +178,7 @@ function renderQuiz(x){
 	console.log('QUIZ RENDERED');
 }
 
-// Next (submit) button
-$('form').submit(function(event){
-	nextQuestion(event);
-});
+
 
 function nextQuestion(){
 	console.log('NEXT');
@@ -225,4 +207,11 @@ $(function() {
 	// Call render functions
 	console.log('CURRENTQ IS: [' + quizItemArr.indexOf(currentQ) + '] ' + currentQ.question);
 	renderQuiz(currentQ);
+	// Disable submit button
+	// $('input[type=submit').prop('disabled', true);
+
+	// Next (submit) button
+	$('form').submit(function(event){
+		nextQuestion(event);
+	});
 });
