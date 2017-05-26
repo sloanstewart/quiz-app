fc49c8972ae7244fccb75a6f144cdb6ae23f4ed6
sloanstewart <sloan_stewart@hotmail.com>
2017-05-26 11:34:12 -0400

    added answer verification

--- app.js
+++ app.js
@@ -1,82 +1,97 @@
 // Create the question and answer objects
 function createQuizItems(){
-	function QuizItem(question, answers) {
+	function QuizItem(question, answers, location) {
 		this.question = question;
 		this.answers = answers;
+		this.location = location;
 	};
 // can create array for all quiz items and not use constructor
 	var intro = new QuizItem('Welcome to this incredible quiz; now shut up and hang on.',
 		['Choose your answer carefully',
 		'There is only one correct answer per question.',
 		'Only the penitent man will pass.',
-		'Good luck!']
+		'Good luck!'],
+		 null
 	);
 
 	var q1 = new QuizItem('What is Murphy\'s Law?',
-		['&quot;The road to success and the road to failure are almost exactly the same.&quot;', '&quot;Anything that can go wrong, will go wrong.&quot;', '&quot;Two wrongs don\'t make a right.&quot;', '&quot;Hope for the best, but prepare for the worst.&quot;']
+		['&quot;The road to success and the road to failure are almost exactly the same.&quot;',
+		'&quot;Anything that can go wrong, will go wrong.&quot;',
+		'&quot;Two wrongs don\'t make a right.&quot;',
+		'&quot;Hope for the best, but prepare for the worst.&quot;'],
+		'1 of 10'
 	);
 
 	var q2 = new QuizItem('How can you tell, without question, if someone is allowed access?',
 		['They will have a proper badge or pass.',
 		'Security will let them in if it\'s ok.',
 		'They look like part of the band.',
-		'They look like they want to be elsewhere.']
+		'They look like they want to be elsewhere.'],
+		'2 of 10'
 	);
 
 	var q3 = new QuizItem('As an audio tech, how can you avoid injury lifting heavy equipment?',
 		['Label all heavy items as &quot;Lighting&quot;.',
 		'Lift with your legs, never your back.',
 		'Have someone help you lift.',
-		'Try harder.']
+		'Try harder.'],
+		'3 of 10'
 	);
 
 	var q4 = new QuizItem('Video content is on the way, when should you test it?',
 		['As soon as possible.',
 		'1 hour before show.',
 		'10 minutes before show.',
-		'Tell the client &quot;No&quot;.']
+		'Tell the client &quot;No&quot;.'],
+		'4 of 10'
 	);
 
 	var q5 = new QuizItem('How do you make money in the music business?',
 		['Hard work and dedication.',
 		'Sell beer.',
 		'Networking -It\'s who you know that counts.',
-		'Knowledge - Folks will value your skills.']
+		'Knowledge - Folks will value your skills.'],
+		'5 of 10'
 	);
 
 	var q6 = new QuizItem('If a patron requests a change to the mix, what should you do?',
 		['Ignore them.',
 		'Pretend to make an adjustment.',
 		'Have security escort them out of the event.',
-		'All of the above.']
+		'All of the above.'],
+		'6 of 10'
 	);
 
 	var q7 = new QuizItem('Lighting is taking a long time to load out, what should you do?',
 		['Jump in and help wrap some cables.',
 		'Try to catch a nap or have a snack.',
 		'Offer to help tear down fixtures.',
-		'Push cases to the truck.']
+		'Push cases to the truck.'],
+		'7 of 10'
 	);
 
 	var q8 = new QuizItem('When should you schedule your lunch or dinner break?',
 		['There is no lunch or dinner break.',
 		'Mid-day.',
 		'During intermission or set change.',
-		'After the event is complete.']
+		'After the event is complete.'],
+		'8 of 10'
 	);
 
 	var q9 = new QuizItem('An artist shows up with a tom drum that is not part of the drum kit, you should:',
 		['Make sure you have a microphone for it.',
 		'Add some low end EQ for maximum impact.',
 		'Throw it in the garbage.',
-		'Make sure it is tuned properly.']
+		'Make sure it is tuned properly.'],
+		'9 of 10'
 	);
 
 	var q10 = new QuizItem('When was the last time you got enough sleep?',
 		['Huh?',
 		'What?',
 		'I dunno.',
-		'Eh.']
+		'Eh.'],
+		'10 of 10'
 	);
 
 	var end = new QuizItem('Well, you made it to the end. See your results below:',[
@@ -90,13 +105,14 @@ function createQuizItems(){
 		q8.question + '</br><span class=youranswer id=q8></span>',
 		q9.question + '</br><span class=youranswer id=q9></span>',
 		q10.question + '</br><span class=youranswer id=q10></span>',
-		]
+		],
+		null
 		// ^ iterate through questions and check CORRECT answers
 	);
 	return [intro, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, end];
 };
 
-// Store Current Question 
+// Store Current Question and Answers
 quizItemArr = createQuizItems();
 var currentQ = quizItemArr[0];
 var yourAnswers = {
@@ -114,6 +130,19 @@ var yourAnswers = {
 	end: null
 };
 
+// var correctAnswers = [
+// 	quizItemArr[1][1],
+// 	quizItemArr[2][3],
+// 	quizItemArr[3][0],
+// 	quizItemArr[4][3],
+// 	quizItemArr.q5[1],
+// 	quizItemArr.q6[3],
+// 	quizItemArr.q7[1],
+// 	quizItemArr.q8[0],
+// 	quizItemArr.q9[2],
+// 	quizItemArr.q10[0, 1, 2, 3],
+// ]
+
 // Render quiz
 	function renderQuestion(item){
 		$('.question').text(item.question);
@@ -128,7 +157,7 @@ var yourAnswers = {
 
 		$('.answer-list').html(answerList);
 
-		// Disable radio selection for intro and end
+		// Disable radio selection for intro and end. Change buttons for those sections.
 		if (currentQ == quizItemArr[0]){
 			console.log('AT INTRO');
 			$('input[type=radio]').attr('disabled', true);
@@ -143,17 +172,38 @@ var yourAnswers = {
 			});
 		}
 
+		// Button should read NEXT for questions
+		else {
+			$('.button').attr('value', 'NEXT');
+			$('.button').attr('disabled', true);
+			$('.button').addClass('disabled');
+		}
+
 		console.log('ANSWER LIST RENDERED');
 	}
 
+
 	function initializeSelection(){
-	// Save answer selection to yourAnswer object
+	// Save answer selection to yourAnswer object and then check it
 	$('.quiz-box').find('input[type=radio]').change(function(){
 		// console.log('SELECTED: ' + $(this).attr('id'));4
+		// Disable selection when one is made and enable button
+		$('input[type=radio]').attr('disabled', true);
+		$('.button').attr('disabled', false);
+		$('.button').removeClass('disabled');
 		q = 'q'+quizItemArr.indexOf(currentQ);
 		yourAnswers[q] = $(this).attr('id');
 		console.log('YOUR ANSWER: ' + yourAnswers[q]);
+		// $('.overlay').addClass('enabled')
+		checkAnswer(yourAnswers[q]);
+		$('.pop-up').addClass('enabled').text('You chose: ' + yourAnswers[q])
 	});
+	}
+
+	function checkAnswer(answer) {
+		if (answer == currentQ){
+
+		}
 	}
 
 	function renderYourAnswers(){
@@ -169,12 +219,17 @@ var yourAnswers = {
 		$('#q10').text(yourAnswers.q10);
 	}
 
+	function renderCounter(){
+		$('.question-counter').html(currentQ.location)
+	}
+
 function renderQuiz(x){
 	createQuizItems();
 	renderQuestion(x);
 	renderAnswerList(x);
 	initializeSelection();
 	renderYourAnswers();
+	renderCounter();
 	console.log('QUIZ RENDERED');
 }
 
@@ -185,6 +240,7 @@ function nextQuestion(){
 	event.preventDefault();
 	findCurrentQ();
 	renderQuiz(currentQ);
+	$('.pop-up').removeClass('enabled')
 };
 
 // Find the question that should be currently displayed
