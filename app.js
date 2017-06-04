efc1f9af36ed7ebfe1877340be9962fc2b8bc32c
Sloan <sloanstewartdev@gmail.com>
2017-06-03 20:35:13 -0400

    refactored a ton of stuff, it's almost working...

--- app.js
+++ app.js
@@ -1,102 +1,130 @@
-var CURRENT_PAGE = 0;
-var QUESTION_TEXT = $('#question');
-var USER_ANSWER = "";
-var BUTTON_SUBMIT = $('#button-submit');
-var STORE = [
-	{
-		// [0] (INTRO)
-		q: 'Welcome to this incredible quiz; now shut up and hang on.',
-		a: ['Choose your answer carefully', 'Only the penitent man will pass.', 'Have fun and good luck!', null],
-		u: null
-	},
-	{
-		// [1]
-		q:'What is Murphy\'s Law?',
-		a: ['&quot;The road to success and the road to failure are almost exactly the same.&quot; ', '&quot;Anything that can go wrong, will go wrong.&quot;', '&quot;Two wrongs don&#39;t make a right.&quot;', '&quot;Hope for the best, but prepare for the worst.&quot;'],
-		u: []
-	},
-	{
-		// [2]
-		q: 'How can you tell, without question, if someone is allowed access?',
-		a: ['They will have a proper badge or pass.', 'Security will let them in if it&#39;s ok.', 'They look like part of the band.', 'They look like they want to be elsewhere.'],
-		u: []
-	},
-	{
-		// [3]
-		q: 'As an audio tech, how can you avoid injury lifting heavy equipment?',
-		a: ['Label all heavy items as &quot;Lighting&quot;.', 'Lift with your legs, never your back.', 'Have someone help you lift.', 'Try harder.'],
-		u: []
-	},
-	{
-		// [4]
-		q: 'Video content is on the way, when should you test it?',
-		a: ['As soon as possible.', '1 hour before show.', '10 minutes before show.', 'Tell the client &quot;No&quot;.'],
-		u: []
-	},
-	{
-		// [5]
-		q: 'How do you make money in the music business?',
-		a: ['Hard work and dedication.', 'Sell beer.', 'Networking -It&#39;s who you know that counts.', 'Knowledge - Folks will value your skills.'],
-		u: []
-	},
-	{
-		// [6]
-		q: 'If a patron requests a change to the mix, what should you do?',
-		a: ['Ignore them.', 'Pretend to make an adjustment.', 'Have security escort them out of the event.', 'All of the above.'],
-		u: []
-	},
-	{
-		// [7]
-		q: 'Lighting is taking a long time to load out, what should you do?',
-		a: ['Jump in and help wrap some cables.', 'Try to catch a nap or have a snack.', 'Offer to help tear down fixtures.', 'Push cases to the truck.'],
-		u: ''
-	},
-	{
-		// [8]
-		q: 'When should you schedule your lunch or dinner break?',
-		a: ['There is no lunch or dinner break.', 'Mid-day.', 'During intermission or set change.', 'After the event is complete.'],
-		u: ''
-	},
-	{
-		// [9]
-		q: 'An artist shows up with a tom drum that is not part of the drum kit, you should:',
-		a: ['Make sure you have a microphone for it.', 'Add some low end EQ for maximum impact.', 'Throw it in the garbage.', 'Make sure it is tuned properly.'],
-		u: ''
-	},
-	{
-		// [10]
-		q: 'When was the last time you got enough sleep?',
-		a: ['Huh?', 'What?', 'I dunno.', 'Eh.'],
-		u: ''
-	},
-	{
-		// [11] (END)
-		q: 'Well, you made it to the end. See your results below:',
-		a: []
-	}
-];
+// currentQuestion, currentUserAnswer, question (q,a,u,c), history
+const STORE = {
+	currentQ: 0,
+	currentUserAnswer: null,
+	questions: [{
+					// [0] (INTRO)
+					q: 'Welcome to this incredible quiz; now shut up and hang on.',
+					a: ['Choose your answer carefully', 'Only the penitent man will pass.', 'Have fun and good luck!', 'suh dude'],
+					u: null,
+					c: null
+				},{
+					// [1]
+					q:'What is Murphy\'s Law?',
+					a: ['"The road to success and the road to failure are almost exactly the same."', '"Anything that can go wrong, will go wrong."', '"Two wrongs don\'t make a right."', '"Hope for the best, but prepare for the worst."'],
+					u: "unanswered",
+					c: 1
+				},{
+					// [2]
+					q: 'How can you tell, without question, if someone is allowed access?',
+					a: ['They will have a proper badge or pass.', 'Security will let them in if it\'s ok.', 'They look like part of the band.', 'They look like they want to be elsewhere.'],
+					u: "unanswered",
+					c: 3
+				},{
+					// [3]
+					q: 'As an audio tech, how can you avoid injury lifting heavy equipment?',
+					a: ['Label all heavy items as \"Lighting\".', 'Lift with your legs, never your back.', 'Have someone help you lift.', 'Try harder.'],
+					u: "unanswered",
+					c: 0
+				},{
+					// [4]
+					q: 'Video content is on the way, when should you test it?',
+					a: ['As soon as possible.', '1 hour before show.', '10 minutes before show.', 'Tell the client \"No\".'],
+					u: "unanswered",
+					c: 3
+				},{
+					// [5]
+					q: 'How do you make money in the music business?',
+					a: ['Hard work and dedication.', 'Sell beer.', 'Networking -It\'s who you know that counts.', 'Knowledge - Folks will value your skills.'],
+					u: "unanswered",
+					c: 1
+				},{
+					// [6]
+					q: 'If a patron requests a change to the mix, what should you do?',
+					a: ['Ignore them.', 'Pretend to make an adjustment.', 'Have security escort them out of the event.', 'All of the above.'],
+					u: "unanswered",
+					c: 3
+				},{
+					// [7]
+					q: 'Lighting is taking a long time to load out, what should you do?',
+					a: ['Jump in and help wrap some cables.', 'Try to catch a nap or have a snack.', 'Offer to help tear down fixtures.', 'Push cases to the truck.'],
+					u: "unanswered",
+					c: 1
+				},{
+					// [8]
+					q: 'When should you schedule your lunch or dinner break?',
+					a: ['There is no lunch or dinner break.', 'Mid-day.', 'During intermission or set change.', 'After the event is complete.'],
+					u: "unanswered",
+					c: 0
+				},{
+					// [9]
+					q: 'An artist shows up with a tom drum that is not part of the drum kit, you should:',
+					a: ['Make sure you have a microphone for it.', 'Add some low end EQ for maximum impact.', 'Throw it in the garbage.', 'Make sure it is tuned properly.'],
+					u: "unanswered",
+					c: 2
+				},{
+					// [10]
+					q: 'When was the last time you got enough sleep?',
+					a: ['Huh?', 'What?', 'I dunno.', 'Eh.'],
+					u: "unanswered",
+					c: [0,1,2,3] //ALL ANSWERS CORRECT
+				},{
+					// [11] (END)
+					q: 'Well, you made it to the end. See your results below:',
+					a: [],
+					u: null,
+					c: null
+				}],
+	answerHistory: [], // Number value of answer, current answer will be last
+};
 
-function renderQA(currentPage){
-	QUESTION_TEXT.text(STORE[currentPage].q); // Render question text
-	for(i = 0 ; i < STORE[currentPage].a.length; i++){ // Render answers to Radio labels
-		$('label[for="answer-'+i+'"] > span').text(STORE[currentPage].a[i]);
-	};
-
-
-	console.log('CURRENT_PAGE: '+CURRENT_PAGE);
-	CURRENT_PAGE = CURRENT_PAGE + 1; // After current page is loaded, increment the count
-
+function renderQA(currentQ, qSelector){
+	qSelector.text(STORE.questions[currentQ].q); // Render question as form legend text
+	STORE.questions[currentQ].a.forEach(function(answer, index){
+		$('label[for="answer-'+index+'"]').text(answer);
+	});
+	initSelection();	
 } 
 
-
-function storeUserAnswer(){
-	var answer = $('input:checked').siblings('span').text();
-	STORE[CURRENT_PAGE].u.push(answer);
-	console.log('STORE['+CURRENT_PAGE+'].u is: '+STORE[1].u+'');
+function initSelection(){ // CHECK FIRST RADIO AND GET IT AS THE CURRENT ANSWER
+	$('input[type=radio]:first').prop('checked', true);
+	getUserAnswer();
 }
 
-function checkUserAnswer(){
+function startListeners(form){ // LISTEN TO FORM FOR ANSWER SELECTION AND SUBMIT
+	form.on('change','input[type=radio]', getUserAnswer)
+		.on('submit', handleSubmit);
+}
 
+function getUserAnswer(event){ // This gets the checked radio and stores it
+	STORE.currentUserAnswer = Number($('input:checked').val());
+	// console.log('currentUserAnswer is: '+STORE.currentUserAnswer);
+	return STORE.currentUserAnswer;
+}
+
+function handleSubmit(event){
+	event.preventDefault();
+	console.log('SUBMIT');
+	storeUserAnswer(getUserAnswer());
+	checkUserAnswer(getUserAnswer());
+	CURRENTQ = STORE.currentQ += 1;
+	Q_SELECTOR = $('#question');
+	renderQA(CURRENTQ, Q_SELECTOR); //Renders the new current page (next page)
+}
+
+function storeUserAnswer(answer){ // Push stored User Answer into the answerHistory array
+	STORE.questions[STORE.currentQ].u = answer;
+	console.log('STORE.questions['+STORE.currentQ+'].u is: '+STORE.questions[STORE.currentQ].u+'');
+}	
+
+function checkUserAnswer(userAnswer){ // check answer and push true if correct, false if incorrect
+	console.log('Checking: '+userAnswer);
+	if(userAnswer == STORE.questions[STORE.currentQ].c){
+		return console.log('CORRECT!');
+	}
+	else {
+		console.log('YA BLEW IT!');
+	}
 }
 
 function results(){ // Match each user a+STORE[1].u+''swer with appropriate question
@@ -109,20 +137,11 @@ function results(){ // Match each user a+STORE[1].u+''swer with appropriate ques
 	return STORE[11].a = resultsArray;
 }
 
-$(function(){
+$(function(){ //DOCUMENT READY!
+	const FORM = $('form');
+	const Q_SELECTOR = $('#question');
+	const CURRENTQ = STORE.currentQ
 	console.log('DOM READY');
-	renderQA(0);
-
-	BUTTON_SUBMIT.click(function(event){
-		event.preventDefault();
-		console.log('SUBMIT');
-		storeUserAnswer();
-		checkUserAnswer();
-		renderQA(CURRENT_PAGE);
-	});
-
-	$('input[type=radio]').change(function(){
-		USER_ANSWER = $('input:checked').siblings('span').text();
-		console.log('USER_ANSWER IS: '+USER_ANSWER);
-	});
+	renderQA(CURRENTQ, Q_SELECTOR);
+	startListeners(FORM);
 });
