6c405a3553cdc3204a2d01274add85ca205faf73
Sloan <sloanstewartdev@gmail.com>
2017-06-05 15:38:57 -0400

    changed filenames for repl.it compatibility

--- index.js
+++ index.js
@@ -1,320 +1,228 @@
-// Create the question and answer objects
-var correctAnswers = [
-	null,
-	'\"Anything that can go wrong, will go wrong.\"',
-	'They look like they want to be elsewhere.',
-	'Label all heavy items as \"Lighting\".',
-	'Tell the client \"No\".',
-	'Sell beer.',
-	'All of the above.',
-	'Try to catch a nap or have a snack.',
-	'There is no lunch or dinner break.',
-	'Throw it in the garbage.',
-	['Huh?', 'What?', 'I dunno.', 'Eh.'],
-	null
-]
+document.documentElement.className = 'no-fouc'; // To prevent FOUC! - see the document ready for more
 
-
-function createQuizItems(){
-	function QuizItem(question, answers, location) {
-		this.question = question;
-		this.answers = answers;
-		this.location = location;
+const STORE = { // currentQuestion, currentUserAnswer, question (q,a,u,c, r), history
+	currentQ: 0,
+	currentUserAnswer: null,
+	questions: [{
+					// [0] (INTRO)
+					q: 'Welcome to this incredible quiz; now shut up and hang on.',
+					a: ['Choose your answer carefully', 'Only the penitent man will pass.', 'Have fun and good luck!'],
+					u: null,
+					c: null,
+					r: null
+				},{
+					// [1]
+					q:'What is Murphy\'s Law?',
+					a: ['"The road to success and the road to failure are almost exactly the same."', '"Anything that can go wrong, will go wrong."', '"Two wrongs don\'t make a right."', '"Hope for the best, but prepare for the worst."'],
+					u: null,
+					c: 1,
+					r: null
+				},{
+					// [2]
+					q: 'How can you tell, without question, if someone is allowed access?',
+					a: ['They will have a proper badge or pass.', 'Security will let them in if it\'s ok.', 'They look like part of the band.', 'They look like they want to be elsewhere.'],
+					u: null,
+					c: 3,
+					r: null
+				},{
+					// [3]
+					q: 'As an audio tech, how can you avoid injury lifting heavy equipment?',
+					a: ['Label all heavy items as \"Lighting\".', 'Lift with your legs, never your back.', 'Have someone help you lift.', 'Try harder.'],
+					u: null,
+					c: 0,
+					r: null
+				},{
+					// [4]
+					q: 'Video content is on the way, when should you test it?',
+					a: ['As soon as possible.', '1 hour before show.', '10 minutes before show.', 'Tell the client \"No\".'],
+					u: null,
+					c: 3,
+					r: null
+				},{
+					// [5]
+					q: 'How do you make money in the music business?',
+					a: ['Hard work and dedication.', 'Sell beer.', 'Networking -It\'s who you know that counts.', 'Knowledge - Folks will value your skills.'],
+					u: null,
+					c: 1,
+					r: null
+				},{
+					// [6]
+					q: 'If a patron requests a change to the mix, what should you do?',
+					a: ['Ignore them.', 'Pretend to make an adjustment.', 'Have security escort them out of the event.', 'All of the above.'],
+					u: null,
+					c: 3,
+					r: null
+				},{
+					// [7]
+					q: 'Lighting is taking a long time to load out, what should you do?',
+					a: ['Jump in and help wrap some cables.', 'Try to catch a nap or have a snack.', 'Offer to help tear down fixtures.', 'Push cases to the truck.'],
+					u: null,
+					c: 1,
+					r: null
+				},{
+					// [8]
+					q: 'When should you schedule your lunch or dinner break?',
+					a: ['There is no lunch or dinner break.', 'Mid-day.', 'During intermission or set change.', 'After the event is complete.'],
+					u: null,
+					c: 0,
+					r: null
+				},{
+					// [9]
+					q: 'An artist shows up with a tom drum that is not part of the drum kit, you should:',
+					a: ['Make sure you have a microphone for it.', 'Add some low end EQ for maximum impact.', 'Throw it in the garbage.', 'Make sure it is tuned properly.'],
+					u: null,
+					c: 2,
+					r: null
+				},{
+					// [10]
+					q: 'When was the last time you got enough sleep?',
+					a: ['Huh?', 'What?', 'I dunno.', 'Eh.'],
+					u: null,
+					c: 'Any', //ALL ANSWERS CORRECT
+					r: 'Correct'
+				},{
+					// [11] (END)
+					q: 'Well, you made it to the end. See your results below:',
+					a: [],
+					u: null,
+					c: null
+				}]
 	};
-// can create array for all quiz items and not use constructor
-	var intro = new QuizItem('Welcome to this incredible quiz; now shut up and hang on.',
-		['Choose your answer carefully',
-		'Only the penitent man will pass.',
-		'Good luck!'],
-		 null
-	);
 
-	var q1 = new QuizItem('What is Murphy\'s Law?',
-		['&quot;The road to success and the road to failure are almost exactly the same.&quot; ',
-		'&quot;Anything that can go wrong, will go wrong.&quot;',
-		'&quot;Two wrongs don&#39;t make a right.&quot;',
-		'&quot;Hope for the best, but prepare for the worst.&quot;'],
-		'1 of 10'
-	);
+function renderQA(){ //Render current question and matching answers to the form
+	var currentQ = STORE.currentQ;
+	$('#count').text('Question '+currentQ+' of '+(STORE.questions.length-2)); // Render question count
+	$('#question').text(STORE.questions[currentQ].q); // Render question as form legend text
+	$('#result').hide(); // Hide result DIV
+	initSelection();	// Select first radio button
 
-	var q2 = new QuizItem('How can you tell, without question, if someone is allowed access?',
-		['They will have a proper badge or pass.',
-		'Security will let them in if it&#39;s ok.',
-		'They look like part of the band.',
-		'They look like they want to be elsewhere.'],
-		'2 of 10'
-	);
-
-	var q3 = new QuizItem('As an audio tech, how can you avoid injury lifting heavy equipment?',
-		['Label all heavy items as &quot;Lighting&quot;.',
-		'Lift with your legs, never your back.',
-		'Have someone help you lift.',
-		'Try harder.'],
-		'3 of 10'
-	);
-
-	var q4 = new QuizItem('Video content is on the way, when should you test it?',
-		['As soon as possible.',
-		'1 hour before show.',
-		'10 minutes before show.',
-		'Tell the client &quot;No&quot;.'],
-		'4 of 10'
-	);
-
-	var q5 = new QuizItem('How do you make money in the music business?',
-		['Hard work and dedication.',
-		'Sell beer.',
-		'Networking -It&#39;s who you know that counts.',
-		'Knowledge - Folks will value your skills.'],
-		'5 of 10'
-	);
-
-	var q6 = new QuizItem('If a patron requests a change to the mix, what should you do?',
-		['Ignore them.',
-		'Pretend to make an adjustment.',
-		'Have security escort them out of the event.',
-		'All of the above.'],
-		'6 of 10'
-	);
-
-	var q7 = new QuizItem('Lighting is taking a long time to load out, what should you do?',
-		['Jump in and help wrap some cables.',
-		'Try to catch a nap or have a snack.',
-		'Offer to help tear down fixtures.',
-		'Push cases to the truck.'],
-		'7 of 10'
-	);
-
-	var q8 = new QuizItem('When should you schedule your lunch or dinner break?',
-		['There is no lunch or dinner break.',
-		'Mid-day.',
-		'During intermission or set change.',
-		'After the event is complete.'],
-		'8 of 10'
-	);
-
-	var q9 = new QuizItem('An artist shows up with a tom drum that is not part of the drum kit, you should:',
-		['Make sure you have a microphone for it.',
-		'Add some low end EQ for maximum impact.',
-		'Throw it in the garbage.',
-		'Make sure it is tuned properly.'],
-		'9 of 10'
-	);
-
-	var q10 = new QuizItem('When was the last time you got enough sleep?',
-		['Huh?',
-		'What?',
-		'I dunno.',
-		'Eh.'],
-		'10 of 10'
-	);
-
-	var end = new QuizItem('Well, you made it to the end. See your results below:',[
-		'01. ' + q1.question + '<br>Correct Answer: <span class=correct-answer id=a1></span>' + '<br>Your Answer: <span class=youranswer id=1></span>',
-		'02. ' + q2.question + '<br>Correct Answer: <span class=correct-answer id=a2></span>' + '<br>Your Answer: <span class=youranswer id=2></span>',
-		'03. ' + q3.question + '<br>Correct Answer: <span class=correct-answer id=a3></span>' + '<br>Your Answer: <span class=youranswer id=3></span>',
-		'04. ' + q4.question + '<br>Correct Answer: <span class=correct-answer id=a4></span>' + '<br>Your Answer: <span class=youranswer id=4></span>',
-		'05. ' + q5.question + '<br>Correct Answer: <span class=correct-answer id=a5></span>' + '<br>Your Answer: <span class=youranswer id=5></span>',
-		'06. ' + q6.question + '<br>Correct Answer: <span class=correct-answer id=a6></span>' + '<br>Your Answer: <span class=youranswer id=6></span>',
-		'07. ' + q9.question + '<br>Correct Answer: <span class=correct-answer id=a7></span>' + '<br>Your Answer: <span class=youranswer id=9></span>',
-		'08. ' + q7.question + '<br>Correct Answer: <span class=correct-answer id=a8></span>' + '<br>Your Answer: <span class=youranswer id=7></span>',
-		'09. ' + q8.question + '<br>Correct Answer: <span class=correct-answer id=a9></span>' + '<br>Your Answer: <span class=youranswer id=8></span>',
-		'10. ' + q10.question + '<br>Correct Answer: <span class=correct-answer id=a10></span>' + '<br>Your Answer: <span class=youranswer id=10></span>',
-		],
-		null
-		// ^ iterate through questions and check CORRECT answers
-	);
-	return [intro, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, end];
-};
-
-// Store Current Question and Answers
-var quizItemArr = createQuizItems();
-var currentQ = quizItemArr[0];
-var yourAnswers = [0];
-
-
-
-// Render quiz
-	function renderQuestion(item){
-		$('.question').text(item.question);
-	}
-
-	function renderAnswerList(item) {
-		var answerList = [];
-		$.each(item.answers, function(i, value){
-			// console.log('answer list value: ' + value);
-			var listHTML = (`
-					<input class="visuallyhidden" type="radio" name="answer" id="radio-${i}" ${(i===0 ? 'checked="checked"': null)}>
-					<label for="radio-${i}">${value}</label>
-				`);
-			var answerHTML = '<li class=answer-container><input class=visuallyhidden type=radio name=answer id="'+value+'" checked='+(i === 0 ? true : false)+'><label for="'+value+'"></span>'+value+'</span></label></li>'
-			answerList.push(listHTML);
+	if(currentQ == 0){ // INTRO
+		$('#count').css('visibility', 'hidden');
+		STORE.questions[currentQ].a.forEach(function(answer, index){ // Render answers to radio labels
+			var introHTML = `<p>${answer}</p>`;
+			$('#intro').append(introHTML);
 		});
-		$('.answer-list').html(answerList);
-
-		// INTRO
-		if (currentQ == quizItemArr[0]){
-			$('input[type=radio]').attr('disabled', true); // Disable radio selection
-			$('#btn-alt').attr('value', 'BEGIN').click(function(){ // Change button value to BEGIN
-				nextQuestion();
-			});
-			console.log('AT INTRO');
-		}
-
-		// END
-		else if (currentQ == quizItemArr[11]){
-			$('input[type=radio]').attr('disabled', true); // Disable radio selection
-			$('#btn-submit').addClass('display-none'); // Hide SUBMIT
-			$('#btn-alt').removeClass('display-none');
-			$('#btn-alt').attr('value', 'RESTART').click(function(){ // Change button to RESTART and link to page to 'refresh'
-				window.location.href='';
-				console.log('RESTART');
-			});
-			console.log('AT END');
-		}
-
-		// QUESTIONS
-		else {
-			// focusStyles();
-			// Display SUBMIT instead of alt buttons
-			$('#btn-alt').addClass('display-none');
-			$('#btn-submit').attr('value', 'SUBMIT').removeClass('display-none');	
-		}
-
-		console.log('LIST RENDERED');
+		$('.radio-item').hide();
+		$('#button-submit').show().text('Begin');
+		$('#button-continue').hide();
 	}
-
-
-	function focusStyles(){
-		// Sets checked and focus for first radio button adds style for li and the parent ul
-		$('input[type=radio]:first').prop('checked', true).focus().closest('ul').addClass('js-focus');;
-		$('input[type=radio]:checked').parent('li').addClass('js-li-focus');
-
-		// Adds custom focus class to UL to indicate entire radio group is in focus
-		$('input[type=radio]').focusin(function(){
-			$('input[type=radio]').closest('ul').addClass('js-focus');
+	else if(currentQ == 11){ // END
+		$('#count').css('visibility', 'hidden');
+		getResults();
+		for (i = 1; i < STORE.questions.length-1; i++){ // Render answers to radio labels
+				var results = {
+					n: i,
+					q: STORE.questions[i].q,
+					c: STORE.questions[i].a[STORE.questions[i].c],
+					u: STORE.questions[i].a[STORE.questions[i].u],
+					r: STORE.questions[i].r
+				}
+			var resultsHTML = `<p class="results-p"><span class="results-number">${results.n}.</span> <span class="results-question">${results.q}</span><br>Correct Answer: <span class="results-correct">${results.c}</span><br>Your Answer: <span class="results-user">${results.u}</span><br>Result: <span class="results-result">${results.r}</span></p>`;
+			$('.radio-item').hide();
+			$('#end-results').hide();
+			$('#end-results').append(resultsHTML);
+			$('#end-results').fadeIn();
+			$('#button-submit').show().text('Restart');
+			$('#button-continue').hide();
+		};
+	}
+	else{
+		$('#count').css('visibility', 'visible');
+		$('#intro').hide();
+		$('.radio-item').fadeIn();
+		STORE.questions[currentQ].a.forEach(function(answer, index){ // Render answers to radio labels
+			$('label[for="answer-'+index+'"]').text(answer);
 		});
-		$('input[type=radio]').focusout(function(){
-			$('input[type=radio]').closest('ul').removeClass('js-focus');
-		});
-		
-		// Toggle visual focus style for selected answer's li
-		$('input[type=radio]').change(function(){
-		$('input[type=radio]:not(:checked)').parent('li').removeClass('js-li-focus');
-		$('input[type=radio]:checked').parent('li').addClass('js-li-focus');
-		});
+		$('#button-submit').show().text('Submit');
+		$('#button-continue').hide();
 	}
+	console.log('CurrentQ is: '+currentQ);
+} 
 
-	//Checks answer and returns result along with colored DIV
-	function checkAnswer(location, answer) {
-		var nextButton = '<input id="btn-next" class="button" type="button" value="Continue">';
-		$('#btn-submit').prop('disabled', true);
-		if (answer == correctAnswers[location]){
-			console.log('CORRECT');
-			$('.pop-up').removeClass('incorrect').addClass('enabled correct').html('<h2>CORRECT</h2> <p>'+correctAnswers[location]+'</p>'+nextButton);
-			$('#btn-next').click(function(){
-				nextQuestion();
-			});
-		}
-
-		else if (location == 10){
-			console.log('ANY ANSWER CORRECT');
-			$('.pop-up').removeClass('incorrect').addClass('enabled correct').html('<h2>CORRECT</h2> <p>All answers are correct; get some sleep already!</p>'+nextButton);
-			$('#btn-next').click(function(){
-				nextQuestion();
-			});
-		}
-
-		else {
-			console.log('INCORRECT');
-			$('.pop-up').removeClass('correct').addClass('enabled incorrect').html('<h2>INCORRECT</h2><p>'+correctAnswers[location]+'</p>'+nextButton);
-			$('#btn-next').click(function(){
-				nextQuestion();
-			});
-		}
-	}
-
-	function renderCorrect(){
-		$('#a1').text(correctAnswers[1]);
-		$('#a2').text(correctAnswers[2]);
-		$('#a3').text(correctAnswers[3]);
-		$('#a4').text(correctAnswers[4]);
-		$('#a5').text(correctAnswers[5]);
-		$('#a6').text(correctAnswers[6]);
-		$('#a7').text(correctAnswers[7]);
-		$('#a8').text(correctAnswers[8]);
-		$('#a9').text(correctAnswers[9]);
-		$('#a10').text(correctAnswers[10]);
-	}
-
-	function renderYourAnswers(){
-		$('#1').text(yourAnswers[1]);
-		$('#2').text(yourAnswers[2]);
-		$('#3').text(yourAnswers[3]);
-		$('#4').text(yourAnswers[4]);
-		$('#5').text(yourAnswers[5]);
-		$('#6').text(yourAnswers[6]);
-		$('#7').text(yourAnswers[7]);
-		$('#8').text(yourAnswers[8]);
-		$('#9').text(yourAnswers[9]);
-		$('#10').text(yourAnswers[10]);
-	}
-
-	
-	function renderCounter(){
-		$('.question-counter').html(currentQ.location)
-	}
-
-
-function renderQuiz(x){
-	createQuizItems();
-	renderQuestion(x);
-	renderAnswerList(x);
-	renderYourAnswers();
-	renderCorrect();
-	renderCounter();
-	console.log('QUIZ RENDERED');
+function initSelection(){ // CHECK FIRST RADIO AND GET IT AS THE CURRENT ANSWER
+	$('input[type=radio]:first').prop('checked', true);
+	getUserAnswer();
 }
 
+function startListeners(form){ // LISTEN TO FORM FOR ANSWER SELECTION AND SUBMIT
+	form.on('change','input[type=radio]', getUserAnswer)
+		.on('submit', handleSubmit);
+}
 
+function getUserAnswer(event){ // This gets the checked radio and stores it
+	STORE.currentUserAnswer = Number($('input:checked').val());
+	return STORE.currentUserAnswer;
+}
 
-function nextQuestion(){
-	console.log('NEXT');
-	findCurrentQ();
-	renderQuiz(currentQ);
-	$('.pop-up').removeClass('enabled')
-	$('#btn-submit').prop('disabled', false);
+function handleSubmit(event){ //When a fool smashes dat SUBMIT, please do the following
+	event.preventDefault();
 
-};
-
-// Find the question that should be currently displayed
-function findCurrentQ(i) {
-	for(var i = 0; i < quizItemArr.length; i++) {
-		if (quizItemArr[i].question == currentQ.question){
-			currentQ = quizItemArr[i + 1];
-			console.log('CURRENTQ IS: [' + quizItemArr.indexOf(currentQ) + '] ' + currentQ.question);
-			return currentQ;
-		}
-		else if (currentQ == quizItemArr[9].question) { //May not need this if restart button works as it should
-			console.log("CURRENTQ AT END OF ARRAY");
-		}
+	if(STORE.currentQ == 11 ){ // If at end, submit will reload entire quiz
+		console.log('RESTART');
+		window.location.href='';
 	}
-};
-
-// Document ready
-$(function() {
-	console.log('DOC READY');
-	// Call render functions
-	console.log('CURRENTQ IS: [' + quizItemArr.indexOf(currentQ) + '] ' + currentQ.question);
-	renderQuiz(currentQ);
-	$('form').submit(function(event){ // SUBMIT BUTTON
-		event.preventDefault();
+	else if(STORE.currentQ == 0){
+		console.log('BEGIN');
+		renderQA(STORE.currentQ += 1); //Renders the new current page (next page)
+	}
+	else{
 		console.log('SUBMIT');
-		yourAnswers.push($('input[type=radio]:checked').attr('id')); // Store answer
-		var location = quizItemArr.indexOf(currentQ); // find location of current question
-		var answer = yourAnswers[location];
-		console.log('YOUR ANSWER: ' + answer);
+		storeUserAnswer(getUserAnswer());
+		checkUserAnswer(getUserAnswer());
+	}
+}
 
-		checkAnswer(location, answer); // Call function to check answer
+function storeUserAnswer(answer){ // Push stored User Answer into the answerHistory array
+	STORE.questions[STORE.currentQ].u = answer;
+}	
+
+function checkUserAnswer(userAnswer){ // check answer and push true if correct, false if incorrect
+	if(STORE.currentQ == 10){ // Any answer is correct for question 10
+		STORE.questions[10].a.push('Any'); // Push a new answer 'Any'
+		STORE.questions[10].c = 4;	// Ensure correct answer will be 'Any'
+		console.log('CORRECT!');
+		displayResult('CORRECT!');
+	}
+	else if(userAnswer == STORE.questions[STORE.currentQ].c){
+		STORE.questions[STORE.currentQ].r = 'Correct';
+		console.log('CORRECT!');
+		$('#result').removeClass('incorrect').addClass('correct');
+		displayResult('CORRECT!')
+	}
+	else {
+		STORE.questions[STORE.currentQ].r = 'Incorrect';
+		console.log('YA BLEW IT!');
+		$('#result').removeClass('correct').addClass('incorrect');
+		displayResult('YA BLEW IT!')
+	}
+}
+
+function displayResult(result){ // Show if User Answer is correct or not
+	$('.radio-item').hide();
+	$('#button-submit').hide();
+	$('#result').fadeIn();
+	$('#result > p').text(result);
+	$('#button-continue').show().unbind('click').click(function(e){
+		e.preventDefault();
+		console.log('currentq +1')
+		renderQA(STORE.currentQ += 1); //Renders the new current page (next page)
+
 	});
+}
+
+function getResults(){ // Match each user answer with appropriate question
+	var resultsArray=[];
+	for(i = 1 ; i < STORE.questions.length-1; i++){
+		var question = STORE.questions[i].q;
+		var answerNum = STORE.questions[i].u;
+		var answerStr = STORE.questions[i].a[answerNum];
+		resultsArray.push( {q:question, a:answerStr} );
+	}
+	return STORE.questions[11].a = resultsArray;
+}
+
+$(function(){ //DOCUMENT READY!
+	renderQA();
+	startListeners($('form'));
+	$('.no-fouc').removeClass('no-fouc');
 });
